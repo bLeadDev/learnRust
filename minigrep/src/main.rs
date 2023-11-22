ebb792bc00c0c29ebc99da18234ff50748d990ff
Michael Geuze <michael.geuze@gmail.com>
2023-11-22 19:53:03 +0100

    Worked on minigrep. At L12-2 right now.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,8 +1,42 @@
 use std::env;
+use std::fs;
+use std::process;
+use std::error::Error;
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str>{
+        if args.len() < 3 {
+            return Err("Too few arguments!");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config { query, file_path })
+    }
+    
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.file_path)?;
+    println!("With text\n{}", contents);
+    Ok(())
+}
 
 fn main() {
-    let arguments: Vec<String> = env::args().collect();
-    for ar in arguments{
-        println!("{}", ar);
-    }
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for: {:#}", config.query);
+    println!("In file : {:#}", config.file_path);
+
+    run(config);   
+
 }
