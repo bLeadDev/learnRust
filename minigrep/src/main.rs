add2929d37410b54dd733d2526f43e1f229576ba
Michael Geuze <michael.geuze@gmail.com>
2023-11-22 20:48:47 +0100

    Finished chapter 12.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,30 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
 
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str>{
-        if args.len() < 3 {
-            return Err("Too few arguments!");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config { query, file_path })
-    }
-    
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("With text\n{}", contents);
-    Ok(())
-}
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -36,7 +13,9 @@ fn main() {
 
     println!("Searching for: {:#}", config.query);
     println!("In file : {:#}", config.file_path);
-
-    run(config);   
-
+   
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
